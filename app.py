08229a87b30ef92c425dea11cc90edc3bf050ac2
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2017-04-01 18:36:37 -0400

    Reverting changes to app.py

--- app.py
+++ app.py
@@ -5,18 +5,5 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
-@app.route("/gabriel")
-def gabriel():
-    return "Hello, Ms. Wright!"
-
-@app.route("/fareed")
-def fareed():
-    return "Hello, Mr.Gabriel!"
-  
-@app.route("/ANAJAVED")
-def ANAJAVED():
-    return "Hello, ANA!"
-
-
 if __name__ == "__main__":
     app.run()
