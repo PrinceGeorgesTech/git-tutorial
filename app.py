365bec3667b11097c25ebee9d8d9046efcc9be93
Buddyerosion <buddyerosion@gmail.com>
2017-04-02 15:12:12 -0400

    Changed the name in the file

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+@app.route("/")
+def hello():
+    return "Hello, World!" 
+@app.route("/Kwame")
+def hello():
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run()
