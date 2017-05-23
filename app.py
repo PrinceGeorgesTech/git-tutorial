318baf08e402d34a9f0163fa04e0bb2314e70d4e
geramirez <gabriel.e.ramirez@gmail.com>
2017-05-23 16:21:16 -0700

    Restart

--- app.py
+++ app.py
@@ -5,50 +5,6 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
-
-@app.route("/Laura")
-def Laura():
-    return "Hello, Laura!"
-
-@app.route("/Peg")
-def myname():
-    return "Hello, My Name is Peg!"
-
-@app.route("/Kwame")
-def Qat():
-    return "Hello, World KwameB!"
-
-@app.route("/chuck")
-def chuck():
-    return "Hello, Chuck!"
-
-@app.route("/Xin")
-def Xin():
-	return "Hello, Xin!"
-
-@app.route("/ruby")
-def ruby():
-    return "Hello, RUBY!"
-
-@app.route("/David")
-def David():
-    return "Hello, David!"
-
-
-@app.route("/leesandra")
-def lee():
-    return "Hello, Lee Sandra!"
-
-
-@app.route("/Kizzie")
-def Kizzie():
-	return "Hello, Kizzie!"
-	
-@app.route("/franklin")
-def franklin():
-    return "Hello, Franklin!"
-
 if __name__ == "__main__":
-
     app.run()
 
