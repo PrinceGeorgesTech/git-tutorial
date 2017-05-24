5dceb2c62b9bc8fe1721e2efc6bebe4cf51a676e
lucas <lucas.yamone@gmail.com>
2017-05-24 19:17:57 -0400

    I have chenged my name!

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/lucas")
+def lucas():
+    return "Hello, Lucas!"
 
 if __name__ == "__main__":
     app.run()
-
